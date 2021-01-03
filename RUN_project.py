931b061f30bccc0d8fd1adec15620cd8e7035b7f
alexhanganu <alexandru.hanganu@umontreal.ca>
2021-01-03 09:51:32 -0500

    Fix link

--- RUN_project.py
+++ RUN_project.py
@@ -10,12 +10,12 @@ STEP3_run_stats          = False
 from os import path, system
 from bin.db_processing import Table
 from bin.vars import VARS
-# from bin import nimb_link
-# NIMB_HOME = nimb_link.link_with_nimb()
-# from setup.get_vars import Get_Vars, SetProject
-# all_vars = Get_Vars()
-# project_vars = all_vars.projects[project]
-# #nimb_stats = SetProject(all_vars.location_vars['local']['NIMB_PATHS']['NIMB_tmp'], all_vars.stats_vars, project).stats
+from bin import nimb_link
+NIMB_HOME = nimb_link.link_with_nimb()
+from setup.get_vars import Get_Vars, SetProject
+all_vars = Get_Vars()
+project_vars = all_vars.projects[project]
+#nimb_stats = SetProject(all_vars.location_vars['local']['NIMB_PATHS']['NIMB_tmp'], all_vars.stats_vars, project).stats
 
 if STEP0_make_groups:
     from bin.step1_make_groups import MakeGroupFile
