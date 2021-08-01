c0d36b9148d7c185f77f6d58ed40147851b81c78
alex hanganu <alexandru.hanganu@umontreal.ca>
2021-08-01 08:28:48 -0400

    adjusting for errs

--- RUN_project.py
+++ RUN_project.py
@@ -5,38 +5,46 @@
 class RUNProject:
 
 	def __init__(self, all_vars):
-		
+		self.project      = all_vars.params.project
+        self.vars_local   = all_vars.location_vars['local']
+        self.project_vars = all_vars.projects[self.project]
+        self.steps        = self.get_steps()
+		STEP0_make_groups        = False
+		STEP1_run_fsglm          = False
+		STEP2_extract_fsglm_img  = True
 
-	STEP0_make_groups        = False
-	STEP1_run_fslgm          = False
-	STEP2_extract_fslgm_img  = True
+    def run(self):
+    	print("running")
 
 
-	if STEP0_make_groups:
+    def get_steps(self):
+
+
+	"""if STEP0_make_groups:
 	    from bin.step1_make_groups import MakeGroupFile
-	    MakeGroupFile(project_vars, utils, Table, Preprocess)
+	    MakeGroupFile(self.project_vars, utils, Table, Preprocess)
 
-	if STEP1_run_fslgm:
+	if STEP1_run_fsglm:
 		FS_SUBJECTS_DIR = all_vars.location_vars['local']['FREESURFER']['FS_SUBJECTS_DIR']
 		from bin.step2_fsglm_run import FSGLMrun
-		ready = FSGLMrun(project_vars, local_vars, save_json, Table, manage_archive, FS_SUBJECTS_DIR)
+		ready = FSGLMrun(self.project_vars, self.vars_local, save_json, Table, manage_archive, FS_SUBJECTS_DIR)
 		if ready:
 			print('ready for FreeSurfer GLM')
 			import os
 			os.chdir(NIMB_HOME)
-			os.system('python3 nimb.py -process fs-glm -project {}'.format(project))
+			os.system('python3 nimb.py -process fs-glm -project {}'.format(self.project))
 		else:
 			print('file for GLM is not ready')
 
-	if STEP2_extract_fslgm_img:
+	if STEP2_extract_fsglm_img:
 		print('ready to extract FreeSurfer GLM images')
 		import os
 		os.chdir(NIMB_HOME)
-		os.system('python3 nimb.py -process fs-glm-image -project {}'.format(project))
+		os.system('python3 nimb.py -process fs-glm-image -project {}'.format(self.project))"""
 
 
 
-def get_parameters():
+def get_parameters(project):
     """get parameters for nimb"""
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -47,7 +55,15 @@ def get_parameters():
 
     parser.add_argument(
         "-step", required=True,
-        help="absolute path to MR data to be classified",
+        choices = [0, 1, 2, 3, 4, 00]
+        help="choice 0 = make groups; 1 = run fsglm; \
+        		2 = extract fsglm images; \
+        		4 = redo grid with ROIs; 00 = run all steps",
+    )
+
+    parser.add_argument(
+        "-project", required=False,
+        default=project,
     )
 
     parser.add_argument(
@@ -72,16 +88,11 @@ if __name__ == "__main__":
 	from distribution.utilities import save_json
 	from distribution import manage_archive
 
-	all_vars = Get_Vars()
-	local_vars   = all_vars.location_vars["local"]
-	project_vars = all_vars.projects[project]
+    params   = get_parameters(project)
+	all_vars = Get_Vars(params)
 
     # from pathlib import Path
     # top = Path(__file__).resolve().parents[1]
     # sys.path.append(str(top))
 
-    params      = get_parameters()
-    DCM2BIDS_tester(params, project).run()
-
-
-
+    RUNProject(all_vars).run()
