ceace506c8af07f53dbf5ebe5a7a6f9cc77edf9d
alex hanganu <alexandru.hanganu@umontreal.ca>
2021-08-02 11:14:53 -0400

    adjusting to make grid based on ROIs

--- RUN_project.py
+++ RUN_project.py
@@ -2,46 +2,73 @@
 # coding: utf-8
 # last update: 20210730
 
+import os
+import argparse
+
 class RUNProject:
 
-	def __init__(self, all_vars):
-		self.project      = all_vars.params.project
+    def __init__(self, all_vars):
+        self.project      = all_vars.params.project
         self.vars_local   = all_vars.location_vars['local']
         self.project_vars = all_vars.projects[self.project]
-        self.steps        = self.get_steps()
-		STEP0_make_groups        = False
-		STEP1_run_fsglm          = False
-		STEP2_extract_fsglm_img  = True
+        self.get_steps(all_vars)
+
 
     def run(self):
-    	print("running")
+        print("running")
+        for step in self.steps:
+            step2run = self.steps[step]['name']
+            if self.steps[step]["run"]:
+                print(f"    running step: {step2run}")
+                self.run_step(step2run)
 
 
-    def get_steps(self):
+    def run_step(self, step2run):
+        if step2run == "STEP0_make_groups":
+            from bin.step1_make_groups import MakeGroupFile
+            MakeGroupFile(self.project_vars, Table, Preprocess)
+
+        if step2run == "STEP1_run_fsglm":
+            FS_SUBJECTS_DIR = all_vars.location_vars['local']['FREESURFER']['FS_SUBJECTS_DIR']
+            from bin.step2_fsglm_run import FSGLMrun
+            ready = FSGLMrun(self.project_vars, self.vars_local, save_json, Table, manage_archive, FS_SUBJECTS_DIR)
+            if ready:
+                print('ready for FreeSurfer GLM')
+                os.chdir(NIMB_HOME)
+                os.system('python3 nimb.py -process fs-glm -project {}'.format(self.project))
+            else:
+                print('    file for GLM is not ready')
+
+        if step2run == "STEP2_extract_fsglm_img":
+            print('ready to extract FreeSurfer GLM images')
+            os.chdir(NIMB_HOME)
+            os.system('python3 nimb.py -process fs-glm-image -project {}'.format(self.project))
+
+        if step2run == "STEP3_ger_ROIs_grid":
+            print('    ready to create ROIs grid')
+            from bin.step1_make_groups import MakeGroupFile
+            MakeGroupFile(self.project_vars,
+                            Table,
+                            Preprocess,
+                            rois = True)
 
 
-	"""if STEP0_make_groups:
-	    from bin.step1_make_groups import MakeGroupFile
-	    MakeGroupFile(self.project_vars, utils, Table, Preprocess)
-
-	if STEP1_run_fsglm:
-		FS_SUBJECTS_DIR = all_vars.location_vars['local']['FREESURFER']['FS_SUBJECTS_DIR']
-		from bin.step2_fsglm_run import FSGLMrun
-		ready = FSGLMrun(self.project_vars, self.vars_local, save_json, Table, manage_archive, FS_SUBJECTS_DIR)
-		if ready:
-			print('ready for FreeSurfer GLM')
-			import os
-			os.chdir(NIMB_HOME)
-			os.system('python3 nimb.py -process fs-glm -project {}'.format(self.project))
-		else:
-			print('file for GLM is not ready')
-
-	if STEP2_extract_fsglm_img:
-		print('ready to extract FreeSurfer GLM images')
-		import os
-		os.chdir(NIMB_HOME)
-		os.system('python3 nimb.py -process fs-glm-image -project {}'.format(self.project))"""
-
+    def get_steps(self, all_vars):
+        self.steps = {
+            "0": {"name" : "STEP0_make_groups",
+                "run" : False},
+            "1": {"name" : "STEP1_run_fsglm",
+                "run" : False},
+            "2": {"name" : "STEP2_extract_fsglm_img",
+                "run" : False},
+            "3": {"name" : "STEP3_ger_ROIs_grid",
+                "run" : False},
+        }
+        if all_vars.params.step == 00:
+            for i in (0, 1, 2, 3, 4):
+                self.steps[i]["run"] = True
+        else:
+            self.steps[all_vars.params.step]["run"] = True
 
 
 def get_parameters(project):
@@ -55,10 +82,13 @@ def get_parameters(project):
 
     parser.add_argument(
         "-step", required=True,
-        choices = [0, 1, 2, 3, 4, 00]
-        help="choice 0 = make groups; 1 = run fsglm; \
-        		2 = extract fsglm images; \
-        		4 = redo grid with ROIs; 00 = run all steps",
+        default='4',
+        choices = ['00', '0', '1', '2', '3'],
+        help="choice ; 00 = run all steps \
+                0 = make groups;\
+                1 = run fsglm;\
+                2 = extract fsglm images;\
+                3 = redo grid with ROIs",
     )
 
     parser.add_argument(
@@ -78,18 +108,18 @@ def get_parameters(project):
 
 if __name__ == "__main__":
 
-	project = "belleville_kenia"
+    project = "belleville_kenia"
 
-	from bin import nimb_link
-	NIMB_HOME = nimb_link.link_with_nimb()
-	from setup.get_vars import Get_Vars, SetProject
-	from stats.db_processing import Table
-	from stats.preprocessing import Preprocess
-	from distribution.utilities import save_json
-	from distribution import manage_archive
+    from bin import nimb_link
+    NIMB_HOME = nimb_link.link_with_nimb()
+    from setup.get_vars import Get_Vars, SetProject
+    from stats.db_processing import Table
+    from stats.preprocessing import Preprocess
+    from distribution.utilities import save_json
+    from distribution import manage_archive
 
     params   = get_parameters(project)
-	all_vars = Get_Vars(params)
+    all_vars = Get_Vars(params)
 
     # from pathlib import Path
     # top = Path(__file__).resolve().parents[1]
--- bin/step1_make_groups.py
+++ bin/step1_make_groups.py
@@ -14,13 +14,12 @@ Return:
     True or False if all steps are correct and finished
 '''
 
-import os
+from os import path
 from .vars import VARS
-
 
 class MakeGroupFile:
 
-    def __init__(self, project_vars, Table, Preprocess, load_json, rois = False, add_date = False):
+    def __init__(self, project_vars, Table, Preprocess, load_json, rois = False):
         self.tab           = Table()
         self.preproc       = Preprocess()
         self.load_json     = load_json
@@ -29,24 +28,39 @@ class MakeGroupFile:
         self.vars          = VARS(project_vars)
         self._id           = project_vars['id_col']
         self.files         = self.vars.f_source()
-        self.miss_val_file = os.path.join(self.materials_DIR, "missing_values.json")
-        self.rois          = rois
-        self.add_date      = add_date
-        self.date          = "20210730"
-        self.run()
+        self.miss_val_file = path.join(self.materials_DIR, "missing_values.json")
+        if not rois:
+            self.run()
+        else:
+            self.make_rois_grid()
 
 
     def run(self):
-        df    = self.get_source_data()
-        df_fs = self.get_freesurfer_data()
-        if self.rois:
-            print("    making rois grid")
-            df_fs = self.get_rois(df_fs)
+        src_file    = self.files['source']
+        src_fs_file = self.files['freesurfer']
+        df    = self.tab.get_df(src_file['file'],
+                                    src_file['sheet'],
+                                    src_file['cols'],
+                                    src_file['ids'])
+        df_fs = self.tab.get_df(src_fs_file['file'],
+                                    src_fs_file['sheet'],
+                                    src_fs_file['cols'],
+                                    src_fs_file['ids'])
         self.grid_df = self.tab.join_dfs(df, df_fs, how='outer')
         self.groups = self.preproc.get_groups(self.grid_df[self.project_vars["group_col"]].tolist())
         self.grid_df.index.name = self._id
-        # self.populate_missing_data()
+        self.populate_missing_data()
         self.create_data_file()
+
+
+    def make_rois_grid(self):
+        print("    making rois grid")
+        f_rois = self.files['rois']['file']
+        if path.exists(f_rois):
+            self.rois = self.load_json(f_rois)
+            print(self.rois)
+        else:
+            print(f"    file with rois is missing: {f_rois}")
 
 
     def populate_missing_data(self):
@@ -56,18 +70,16 @@ class MakeGroupFile:
         """
         self.populate_exceptions()
         _, cols_with_nans = self.tab.check_nan(self.grid_df, self.miss_val_file)
-        if cols_with_nans:
-            print(f"    some columns have missing data and will be adjusted with the mean of the group: {cols_with_nans}")
         df_groups = dict()
         for group in self.groups:
             df_group = self.tab.get_df_per_parameter(self.grid_df, self.project_vars['group_col'], group)
             df_group = self.preproc.populate_missing_vals_2mean(df_group, cols_with_nans)
             df_groups[group] = df_group
         frames = (df_groups[i] for i in df_groups)
-        df_meaned_vals = self.tab.concat_dfs(frames, ax=1, sort = True)
-        # df_meaned_vals = pd.concat(frames, axis=0, sort=True)
+        df_meaned_vals = pd.concat(frames, axis=0, sort=True)
         for col in cols_with_nans:
             self.grid_df[col] = df_meaned_vals[col]
+
 
     def populate_exceptions(self):
         exceptions = self.vars.values_exception()
@@ -76,37 +88,7 @@ class MakeGroupFile:
                 self.grid_df.at[ix, col] = exceptions[ix][col]
 
 
-    def get_rois(self, df_fs):
-        f_rois = self.files['rois']['file']
-        if os.path.exists(f_rois):
-            rois    = self.load_json(f_rois)
-            fs_rois = [i for k in rois.values() for i in k]
-            return self.tab.get_df_from_df(df_fs, fs_rois)
-        else:
-            print(f"    file with rois is missing: {f_rois}")
-            return df_fs
-
-
-    def get_source_data(self):
-        src_file    = self.files['source']
-        return self.tab.get_df(src_file['file'],
-                                src_file['sheet'],
-                                src_file['cols'],
-                                src_file['ids'])
-
-
-    def get_freesurfer_data(self):
-        src_fs_file = self.files['freesurfer']
-        return self.tab.get_df(src_fs_file['file'],
-                                src_fs_file['sheet'],
-                                src_fs_file['cols'],
-                                src_fs_file['ids'])
-
-
     def create_data_file(self):
-        file_path_name = os.path.join(self.materials_DIR, self.project_vars["fname_groups"])
-        if self.add_date:
-            filename, extension = os.path.splitext(file_path_name)
-            file_path_name = f"{filename}_{self.date}{extension}"
+        file_path_name = path.join(self.materials_DIR, self.project_vars["GLM_file_group"])
         print('creating file with groups {}'.format(file_path_name))
         self.tab.save_df(self.grid_df, file_path_name, sheet_name = 'grid')
